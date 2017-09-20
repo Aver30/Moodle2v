4068906831600165fa7c6f7b57b85b7b8adc9c2e
Aakash97 <axethesky@gmail.com>
2017-09-20 22:54:39 +1000

    Setting up adding dem page

--- forms.py
+++ forms.py
@@ -2,6 +2,12 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
+class SignupForm(Form):
+  first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+  last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+  email = StringField('Email', validators=[DataRequired("Please enter your email address.")])
+  password = StringField('Password', validators=[DataRequired("Please enter a password.")])
+  submit = SubmitField('Sign up')
 
 class LoginForm(Form):
   email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
-from forms import LoginForm
+from forms import LoginForm, SignupForm
 
 
 app = Flask(__name__)
@@ -61,6 +61,23 @@ def login():
 
     elif request.method == 'GET':
         return render_template('login.html', form=form)
+
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    form = SignupForm
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, 'D',form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+
+            return redirect(url_for('Lec_pageV2'))
+
+    elif request.method == "GET":
+    return render_template('signup.html', form=form)
 
 
 
